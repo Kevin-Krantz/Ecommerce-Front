5ce3ce606519198f7f3a27fe57deb3cfa384b992
Kevin Krantz <kevinkrantz95@hotmail.com>
2023-10-10 01:43:27 +0200

    feat(types): Add IProduct interface for product schema
    - Introduce IProduct interface extending mongoose Document
    
    - Define basic product properties: title, price, etc.
    
    - Optional fields for description, images, category, and properties

---
+++ types/IProduct.ts
@@ -0,0 +1,10 @@
+import { Document } from "mongoose";
+
+export interface IProduct extends Document {
+  title: string;
+  price: number;
+  description?: string;
+  images?: string[];
+  category?: string;
+  properties?: { [key: string]: any };
+}
