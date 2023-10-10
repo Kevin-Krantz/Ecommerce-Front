5e7ec80569f069ea70d7cc1887dea80dd6ed6404
Kevin Krantz <kevinkrantz95@hotmail.com>
2023-10-10 05:35:27 +0200

    feat(components): Enhance Header component layout and styling
    - Introduce the Lekbubblan logo with SVG
    
    - Use Center component for centralized alignment
    
    - Update header background color and other style elements
    
    - Refactor navigation links for consistency and clarity

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,22 +2,63 @@
 
 import Link from "next/link";
 import styled from "styled-components";
+import Center from "./Center";
 
 export default function Header() {
   return (
     <StyledHeader>
-      <Link href="/">Lekbubblan</Link>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/products">All products</Link>
-        <Link href="/categories">Categories</Link>
-        <Link href="/account">Account</Link>
-        <Link href="/cart">Cart (0)</Link>
-      </nav>
+      <Center>
+        <Wrapper>
+          <LogoWrapper>
+            <Svg src="/toy.svg" />
+            <Logo href="/">Lekbubblan</Logo>
+          </LogoWrapper>
+          <StyledNav>
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/products">All products</NavLink>
+            <NavLink href="/categories">Categories</NavLink>
+            <NavLink href="/account">Account</NavLink>
+            <NavLink href="/cart">Cart (0)</NavLink>
+          </StyledNav>
+        </Wrapper>
+      </Center>
     </StyledHeader>
   );
 }
 
 const StyledHeader = styled.header`
-  background-color: #222;
+  background-color: #191716;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 0;
+  align-items: center;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Svg = styled.img`
+  width: 70px;
+  height: auto;
+`;
+
+const Logo = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 16px;
+`;
+
+const NavLink = styled(Link)`
+  color: #aaa;
+  text-decoration: none;
 `;
