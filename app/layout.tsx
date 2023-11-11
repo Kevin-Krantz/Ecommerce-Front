da30d00a33fecd723f88f89837866758a17a5cf1
Kevin Krantz <kevinkrantz95@hotmail.com>
2023-11-11 21:11:23 +0100

    Integrate CartContextProvider into RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,24 @@
 import StyledComponentsRegistry from "@/lib/registry";
 import GlobalStyles from "@/styles/GlobalStyles";
 import { Poppins } from "next/font/google";
+import { CartContextProvider } from "./components/CartContext";
 
 const poppins = Poppins({
   weight: ["400", "500", "700", "900", "100", "200", "300"],
   subsets: ["latin"],
 });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>
+    <html lang="en" className={poppins.className}>
+      <body>
         <StyledComponentsRegistry>
           <GlobalStyles />
-          {children}
+          <CartContextProvider>{children}</CartContextProvider>
         </StyledComponentsRegistry>
       </body>
     </html>
