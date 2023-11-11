e7a5f2c922f892865fd9d983eaac73018daa7d6b
Kevin Krantz <kevinkrantz95@hotmail.com>
2023-11-11 21:15:52 +0100

    Refactor Home component to use useProducts hook for data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,19 @@
-import Product from "@/models/Product";
+"use client";
+
+import { useProducts } from "@/hooks/useProducts";
 import FeaturedProduct from "./components/FeaturedProduct";
 import Header from "./components/Header";
-import { mongooseConnect } from "@/lib/mongoose";
 import NewProducts from "./components/NewProducts";
+import { IProduct } from "@/types/IProduct";
 
-async function getData() {
-  const featuredProductId = "64eea380ca2216b7fd81e720";
-  await mongooseConnect();
-  const featuredProduct = await Product.findById(featuredProductId);
-  const newProducts = await Product.find({}, null, {
-    sort: { _id: -1 },
-    limit: 10,
-  });
-
-  return {
-    featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-    newProducts: JSON.parse(JSON.stringify(newProducts)),
-  };
-}
-
-export default async function Home() {
-  const { featuredProduct, newProducts } = await getData();
+export default function Home() {
+  const { products, featured } = useProducts();
 
   return (
     <div>
       <Header />
-      <FeaturedProduct featuredProduct={featuredProduct} />
-      <NewProducts newProducts={newProducts} />
+      <FeaturedProduct featuredProduct={featured as IProduct} />
+      <NewProducts newProducts={products} />
     </div>
   );
 }
