f3ab0e87386172954dbb56e367415eebb108c1ee
Kevin <kevinkrantz95@hotmail.com>
2024-01-07 01:49:37 +0100

    feat(ProductsPage): Add Footer and localize title
    Enhanced the ProductsPage by adding a Footer component for a complete page layout and localized the Title to 'Alla produkter' for Swedish users. Also, added padding to the Title for improved visual spacing. These changes aim to improve the user navigation experience and cater to a specific language audience.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,6 +6,7 @@ import { useProducts } from "@/hooks/useProducts";
 import ProductsGrid from "../components/ProductsGrid";
 import { IProduct } from "@/types/IProduct";
 import Title from "../components/Title";
+import Footer from "../components/Footer";
 
 export default function ProductsPage() {
   const { allProducts } = useProducts();
@@ -14,9 +15,10 @@ export default function ProductsPage() {
     <>
       <Header />
       <Center>
-        <Title>All Products</Title>
+        <Title style={{ padding: "16px" }}>Alla produkter</Title>
         <ProductsGrid products={allProducts as IProduct[]} />
       </Center>
+      <Footer />
     </>
   );
 }
