8f50670922d0d7f72403d5083143e439a6fc387f
Kevin <kevinkrantz95@hotmail.com>
2024-01-07 01:37:54 +0100

    refactor(NewProducts): Localize title to Swedish
    Updated the title in the NewProducts component from 'New Arrivals' to 'Nya Produkter' to cater to Swedish-speaking users. This change aligns with the ongoing efforts to localize the application for a more region-specific user experience.

--- app/components/NewProducts.tsx
+++ app/components/NewProducts.tsx
@@ -11,7 +11,7 @@ interface Props {
 export default function NewProducts({ newProducts }: Props) {
   return (
     <Center>
-      <Title>New Arrivals</Title>
+      <Title>Nya Produkter</Title>
       <ProductsGrid products={newProducts} />
     </Center>
   );
