74347cb6036be619eaf385255334ec9659877321
Kevin <kevinkrantz95@hotmail.com>
2024-01-16 00:41:04 +0100

    feat(ProductsPage): Add MobileFooter and LoadingSpinner for loading state

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,17 +7,39 @@ import ProductsGrid from "../components/ProductsGrid";
 import { IProduct } from "@/types/IProduct";
 import Title from "../components/Title";
 import Footer from "../components/Footer";
+import MobileFooter from "../components/MobileFooter";
+import LoadingSpinner from "../components/icons/LoadingSpinner";
 
 export default function ProductsPage() {
   const { allProducts } = useProducts();
+
+  if (!allProducts) {
+    return (
+      <>
+        <Header />
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <LoadingSpinner color="#000" />
+        </div>
+        <MobileFooter />
+      </>
+    );
+  }
 
   return (
     <>
       <Header />
       <Center>
-        <Title style={{ padding: "16px" }}>Alla produkter</Title>
+        <Title>Alla produkter</Title>
         <ProductsGrid products={allProducts as IProduct[]} />
       </Center>
+      <MobileFooter />
       <Footer />
     </>
   );
