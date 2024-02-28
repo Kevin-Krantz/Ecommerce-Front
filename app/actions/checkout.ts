07a51626611c596877b57175f3906f78de4c249b
Kevin <kevinkrantz95@hotmail.com>
2024-02-28 03:14:27 +0100

    refactor(checkout): Adapt checkout process for Swedish market and enhance error handling

--- app/actions/checkout.ts
+++ app/actions/checkout.ts
@@ -5,6 +5,7 @@ import Order from "@/models/Order";
 import Product from "@/models/Product";
 import { revalidatePath } from "next/cache";
 import Stripe from "stripe";
+import { Error } from "mongoose";
 
 const stripe = new Stripe(process.env.STRIPE_SK, {
   apiVersion: "2023-10-16",
@@ -14,9 +15,7 @@ export default async function checkout(formData: FormData) {
   await mongooseConnect();
 
   const entries = Object.fromEntries(formData.entries());
-
-  const { name, email, city, postalCode, streetAddress, country } = entries;
-
+  const { name, email, phone, postalCode, streetAddress, town } = entries;
   const products = (entries.products as string).split(",");
 
   const uniqueProductIds = [...new Set(products)];
@@ -34,7 +33,7 @@ export default async function checkout(formData: FormData) {
       line_items.push({
         quantity,
         price_data: {
-          currency: "usd",
+          currency: "sek",
           product_data: { name: productInfo.title },
           unit_amount: productInfo.price * 100,
         },
@@ -44,25 +43,37 @@ export default async function checkout(formData: FormData) {
 
   revalidatePath("/cart");
 
-  const orderDoc = await Order.create({
-    line_items,
-    name,
-    email,
-    city,
-    postalCode,
-    streetAddress,
-    country,
-    paid: false,
-  });
+  try {
+    const orderDoc = await Order.create({
+      line_items,
+      name,
+      email,
+      phone,
+      postalCode,
+      streetAddress,
+      town,
+      paid: false,
+    });
 
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: "payment",
-    customer_email: email as string,
-    success_url: process.env.PUBLIC_URL + "/cart?success=1",
-    cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
-    metadata: { orderId: orderDoc._id.toString() },
-  });
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      customer_email: email as string,
+      success_url: process.env.PUBLIC_URL + "/cart?success=1",
+      cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
+      metadata: { orderId: orderDoc._id.toString() },
+    });
 
-  return { url: session.url };
+    return { url: session.url };
+  } catch (error) {
+    if (error instanceof Error.ValidationError) {
+      const messages = Object.values(error.errors).map((err) =>
+        err.message.toLowerCase()
+      );
+
+      return {
+        errors: messages,
+      };
+    }
+  }
 }
