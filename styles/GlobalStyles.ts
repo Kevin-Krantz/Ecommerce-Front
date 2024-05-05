e89893e523e8b68521fb7cc88b8c78992b11920a
Kevin Krantz <kevinkrantz95@hotmail.com>
2024-05-06 00:19:50 +0200

    fix(global-styles): adjust footer position and improve global styling

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -1,11 +1,35 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-body {
-    background-color: #eee;
-    padding: 0;
-    margin: 0;
+* {
+  margin: 0;
 }
+
+html {
+  position: relative;
+    min-height: 100%;
+}
+
+ html, body {
+    padding: 0;
+    background-color: #eee;
+
+    @media only screen and (max-width: 600px) {
+    background-color: unset;
+  }
+  }
+
+  body {
+     margin: 0 0 286px;
+
+     @media only screen and (max-width: 600px) {
+      margin: unset;
+  }
+  }
+
+  #__next {
+    
+  }
 `;
 
 export default GlobalStyles;
